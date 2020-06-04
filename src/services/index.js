9791cd18b620ac41d67f13062a5ec1fea0c4df0d
ferraripuppi <habin@hotmail.co.uk>
2020-06-04 15:16:52 +0100

    sets up server + heroku deployment pipeline

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,5 @@
 import axios from 'axios';
 
-// export const postUser = async (user) => {
-//   const url = 'https://yo7dm2sa2i.execute-api.eu-west-2.amazonaws.com/prod/signup';
-//   const response = await axios.post(url, {
-//     name: user.name,
-//     email: user.email
-//   });
-//   console.log(response);
-//   return response;
-// };
-
 export const getShowData = async () => {
   // const url = 'https://api.spotify.com/v1/shows/{id}'
   const url = 'https://api.spotify.com/v1/shows/6RNwASBcNjuK4tuqdaXzBn?si=TOQk88gOSUW3zVSvr4mpVg';
